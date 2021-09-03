c8243d3ac8c0aad92bc8672ad6558e7dc7cfea36
TheWarrior7210 <30708997+TheWarrior7210@users.noreply.github.com>
2021-09-03 07:15:25 -0700

    starting to learn components in React

---
+++ React Components/firstReactComponent.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+/*
+Hello World, Part II... THE COMPONENT
+..
+*/
+class MyComponentClass extends React.Component {
+	render() {
+	  return <h1>Hello world</h1>;
+	}
+      };
+       
+ReactDOM.render(
+	<MyComponentClass />,
+	document.getElementById('app')
+);
+
+/*
+Import React
+..
+*/
+import React from 'react';
+
+const React = react;
+
+/*
+Import React DOM
+..
+*/
+onst React = react;
+const ReactDOM = react-dom;
