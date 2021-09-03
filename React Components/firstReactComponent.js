d0ebd4a08e23f10e134a25e619c2f3a45a75a27e
TheWarrior7210 <30708997+TheWarrior7210@users.noreply.github.com>
2021-09-03 09:39:03 -0700

    finishing to learn basic components

--- React Components/firstReactComponent.js
+++ React Components/firstReactComponent.js
@@ -27,5 +27,63 @@ const React = react;
 Import React DOM
 ..
 */
-onst React = react;
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const React = react;
 const ReactDOM = react-dom;
+
+/*
+Create a Component Class
+..
+*/
+const React = react;
+const ReactDOM = react-dom;
+
+class x extends React.Component {};
+
+/*
+Name a Component Class
+...
+*/
+const React = react;
+const ReactDOM = react-dom;
+
+class MyComponentClass extends React.Component {}
+
+/*
+The Render Function
+..
+*/
+const React = react;
+const ReactDOM = react-dom;
+
+class MyComponentClass extends React.Component {
+  render(){
+    return <h1>Hello world</h1>;
+  }
+}
+
+/*
+Create a Component Instance
+...
+*/
+class MyComponentClass extends React.Component {
+	render() {
+	  return <h1>Hello world</h1>;
+	}
+      }
+      
+<MyComponentClass />
+
+/*
+Render a Component
+...
+*/
+class MyComponentClass extends React.Component {
+	render() {
+	  return <h1>Hello world</h1>;
+	}
+      }
+      
+ReactDOM.render(<MyComponentClass />,document.getElementById('app'));
